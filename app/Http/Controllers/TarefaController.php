1f4ec214230b61f4bea35e3571e725748128a6d6
eiisafernandes <isaafernandes2018@gmail.com>
2025-03-20 09:21:55 -0300

    adicionado migration, model e controller da Tarefa

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    //o $id vai ser do tipo string, o $request vai ser do tipo request; ou seja serve pra definir o tipo do objeto
+    public function update(Request $request, string $id)
+    {
+
+    }
+
+    public function destroy(string $id)
+    {
+
+    }
+
+}
